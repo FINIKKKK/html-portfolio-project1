52b99c803c8dc98dd6e4f6f975fd34ba2421bbc6
DIGIKRASH <79862227+digitalKrash@users.noreply.github.com>
2021-08-21 14:48:07 +0500

    Начало

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// Jquery ----------------
+@@include('../js/jquery-3.6.0.min.js');
+
+
+// Plugins ----------------
+@@include('../js/slick.min.js');
+@@include('../js/vanilla-tilt.min.js');
+@@include('../js/wow.min.js');
+
+
+// Main-JS -----------------
+@@include('../js/main.js');
+
+
